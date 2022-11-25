a50441d538790b11bfffb1b26523fc138f1bae54
Roman Wind <119047529+RomanWind@users.noreply.github.com>
2022-11-25 19:06:17 +0100

    Added enemy, Updated gitignore
    Created enemy movement animations, added enemy path to target system, git ignore file fixed to ignore unnesesary folders

---
+++ Tower Defence/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 1.15f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            wavepointIndex++;
+            target = Waypoints.waypoints[wavepointIndex];
+        }
+    }
+}
